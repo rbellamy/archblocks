a4754cc71a96c23b8ff3894899ef0b8ec4244eb8
G. Richard Bellamy <rbellamy@terradatum.com>
2013-06-19 18:17:58 -0700

    Unless the desktop manager is installed first, the xorg.conf.d directory may not exist for the xorg intel video conf.

--- blocks/video/video_intel.sh
+++ blocks/video/video_intel.sh
@@ -2,9 +2,12 @@
 
 # video - intel
 
-_installpkg xf86-video-intel lib32-intel-dri libva-intel-driver lib32-intel-dri 
+_installpkg xf86-video-intel lib32-intel-dri libva-intel-driver
 
 echo "options i915 modeset=1" > /etc/modprobe.d/i915.conf
+
+# unless the desktop manager is installed first, the directory may not exist
+[ -d /etc/X11/xorg.conf.d ] || mkdir -p /etc/X11/xorg.conf.d
 
 cat > /etc/X11/xorg.conf.d/20-intel.conf << EOF
 Section "Device"
