d43d7346b9bb8fdeabd436033de64a39c7cdf8db
G. Richard Bellamy <rbellamy@terradatum.com>
2013-06-19 17:49:53 -0700

    systemd_default should behave better - no need to install sysvinit or deprecated (and no longer available) initscripts.

--- blocks/common/systemd_default.sh
+++ blocks/common/systemd_default.sh
@@ -3,13 +3,13 @@
 # INIT - systemd, pure
 # as per the Arch Linux wiki page on systemd: https://wiki.archlinux.org/index.php/Systemd
 
-_installpkg systemd
-pacman -R --noconfirm sysvinit
-pacman -S --noconfirm systemd-sysvcompat
-pacman -R --noconfirm initscripts # going for pure systemd
+_installpkg systemd systemd-sysvcompat
 
 # NOT NEEDED IN PURE SYSTEMD MODE
 # KERNEL_PARAMS="${KERNEL_PARAMS:+${KERNEL_PARAMS} }init=/bin/systemd"
 
 systemctl enable syslog-ng.service
 
+# persistent journal, self limits to default 10% of volume capacity
+# https://wiki.archlinux.org/index.php/Systemd#Systemd_Journal
+mkdir -p /var/log/journal
